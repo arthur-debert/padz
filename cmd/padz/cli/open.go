f9cd5b9cefa7693defa7d3b8006cecd245ed6670
Arthur Debert <adebert@astron.is>
2025-09-11 14:18:50 -0300

    feat: migrate open command to StoreManager
    - Add OpenWithStoreManager and OpenLazyWithStoreManager functions
    - Use ResolveScratchWithStoreManager for scratch resolution
    - Support updating scratch in the correct store after editing
    - Update CLI to use StoreManager approach
    - Remove legacy store initialization and unused imports
    - Handle empty content by delegating to DeleteWithStoreManager

--- cmd/padz/cli/open.go
+++ cmd/padz/cli/open.go
@@ -6,8 +6,6 @@ package cli
 import (
 	"github.com/arthur-debert/padz/pkg/commands"
 	"github.com/arthur-debert/padz/pkg/output"
-	"github.com/arthur-debert/padz/pkg/project"
-	"github.com/arthur-debert/padz/pkg/store"
 	"github.com/rs/zerolog/log"
 	"os"
 
@@ -23,34 +21,18 @@ func newOpenCmd() *cobra.Command {
 		Long:    OpenLong,
 		Args:    cobra.ExactArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
-			all, _ := cmd.Flags().GetBool("all")
 			global, _ := cmd.Flags().GetBool("global")
 			lazy, _ := cmd.Flags().GetBool("lazy")
-
-			s, err := store.NewStore()
-			if err != nil {
-				log.Fatal().Err(err).Msg("Operation failed")
-			}
 
 			dir, err := os.Getwd()
 			if err != nil {
 				log.Fatal().Err(err).Msg("Operation failed")
 			}
 
-			proj, err := project.GetCurrentProject(dir)
-			if err != nil {
-				log.Fatal().Err(err).Msg("Operation failed")
-			}
-
-			// Run discovery before opening
-			if err := s.RunDiscoveryBeforeCommand(); err != nil {
-				log.Warn().Err(err).Msg("Failed to run discovery")
-			}
-
 			if lazy {
-				err = commands.OpenLazy(s, all, global, proj, args[0])
+				err = commands.OpenLazyWithStoreManager(dir, global, args[0])
 			} else {
-				err = commands.Open(s, all, global, proj, args[0])
+				err = commands.OpenWithStoreManager(dir, global, args[0])
 			}
 
 			// Format output
